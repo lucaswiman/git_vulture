e400cc5b611ac7768181d41f4f042bd4a6397540
Lucas Wiman <lucaswiman@counsyl.com>
2014-09-19 23:50:39 -0700

    typos

--- git_vulture/git_vulture.py
+++ git_vulture/git_vulture.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
             # output it.
 
             print(
-                "{path}:{item.lineno}: Unused {item.typ} '{item}' "
+                "{path}:{item.lineno}: Unused {item.type} '{item}' "
                 "(used {num_found} times in git-grep)".format(
                     item=item, path=pretty_path, num_found=num_found))
         else:
@@ -88,7 +88,7 @@ if __name__ == '__main__':
             # files or tests. Only output if in verbose mose.
             if options.verbose:
                 print("SKIPPING DUE TO USAGES: "
-                    "{path}:{item.lineno}: Unused {item.typ} '{item}' "
+                    "{path}:{item.lineno}: Unused {item.type} '{item}' "
                     "(used {num_found} times in git-grep)".format(
                         item=item, path=pretty_path, num_found=num_found))
 
